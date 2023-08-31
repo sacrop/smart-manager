a3d0852edd30595de97c7db161c1032070c694a7
sidharth ram <acnellithave@gmail.com>
2023-08-31 20:01:50 +0530

    third commit

--- src/main/java/com/useradmin/management/controller/Admincontroller.java
+++ src/main/java/com/useradmin/management/controller/Admincontroller.java
@@ -1,12 +1,32 @@
 package com.useradmin.management.controller;
 
+import java.security.Principal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.useradmin.management.model.UserDtls;
+import com.useradmin.management.repository.UserRepository;
 
 @Controller
 @RequestMapping("/admin")
 public class Admincontroller {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @ModelAttribute
+    public void userDtails(Model m,Principal p)
+    {
+        String email=p.getName();
+        UserDtls user=userRepo.findByEmail(email);
+        m.addAttribute("user", user);
+
+    }
     @GetMapping("/")
     public String home(){
         return "/admin/home";
--- src/main/java/com/useradmin/management/controller/homecontroller.java
+++ src/main/java/com/useradmin/management/controller/homecontroller.java
@@ -1,7 +1,10 @@
 package com.useradmin.management.controller;
 
+import java.security.Principal;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import com.useradmin.management.helper.Message;
 import com.useradmin.management.model.UserDtls;
+import com.useradmin.management.repository.UserRepository;
 import com.useradmin.management.service.UserService;
 
 import jakarta.servlet.http.HttpSession;
@@ -31,6 +35,21 @@ public class homecontroller {
     public String register(){
         return "register";
     }
+
+    @Autowired
+    private UserRepository userRepo;
+
+    @ModelAttribute
+    public void userDtails(Model m,Principal p)
+    {
+        if(p!=null){
+            String email=p.getName();
+        UserDtls user=userRepo.findByEmail(email);
+        m.addAttribute("user", user);
+        }
+
+    }
+
 
     @PostMapping("/createUser")
     public String createUser(@ModelAttribute UserDtls user,@RequestParam(value="agreement",defaultValue = "false") boolean agreement,HttpSession session)
